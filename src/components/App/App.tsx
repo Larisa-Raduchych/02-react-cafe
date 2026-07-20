fb5caa5bbd2a6fea03674cf677788f9a07f2d304
Lari Lari <larisaraduchych@gmail.com>
2026-07-20 16:33:03 +0300

    all tasks

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import CafeInfo from '../CafeInfo/CafeInfo';
 import type { Votes, VoteType } from '../../types/votes';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
@@ -16,11 +17,30 @@ export default function App() {
     setVotes({ good: 0, neutral: 0, bad: 0 });
   };
 
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
   return (
     <div className={css.app}>
       <CafeInfo />
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
-      <VoteStats votes={votes} totalVotes={0} positiveRate={0} />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+    />
+
+    {totalVotes > 0 ? (
+      <VoteStats
+    votes={votes}
+    totalVotes={totalVotes}
+    positiveRate={positiveRate}
+  />
+) : (
+  <Notification />
+)}
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,4 @@
 import { createRoot } from 'react-dom/client';
 import App from './components/App/App';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <div>
-    <h1>Welcome to React</h1>
-    <p>
-      <App />
-    </p>
-  </div>
-);
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
