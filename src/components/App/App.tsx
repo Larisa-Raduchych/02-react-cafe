029d027b68f5860ffa128c167896e5d1907b517a
Lari Lari <larisaraduchych@gmail.com>
2026-07-20 15:17:03 +0300

    task-5

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,26 @@
 import { useState } from 'react';
 import css from './App.module.css';
 import CafeInfo from '../CafeInfo/CafeInfo';
+import type { Votes, VoteType } from '../../types/votes';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
 
 export default function App() {
-  return <div className={css.app}><CafeInfo /></div>;
-}
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
 
+  const handleVote = (type: VoteType) => {
+    setVotes(prev => ({ ...prev, [type]: prev[type] + 1 }));
+  };
+
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+      <VoteStats votes={votes} totalVotes={0} positiveRate={0} />
+    </div>
+  );
+}
